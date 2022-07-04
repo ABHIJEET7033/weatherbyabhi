3c93a586c54b13a7d2364daf8b9fb4d7e76157e7
Abhijeet <2872000abhijeet@gmail.com>
2022-07-04 12:00:57 +0530

    fina

---
+++ src/app.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const path=require('path');
+const hbs = require("hbs");
+
+const app=express();
+const port= process.env.port || 3000;
+app.set("view engine", "hbs");
+
+const staticPath = path.join(__dirname, "../public")
+app.use(express.static(staticPath))
+
+const templatePath = path.join(__dirname, "../templates/views")
+app.set("views", templatePath)
+
+const partialsPath = path.join(__dirname, "../templates/partials")
+hbs.registerPartials(partialsPath)
+
+
+
+
+app.get("",(req,res) =>{
+     res.render('index');
+});
+
+app.get("/about", (req,res) =>{
+    res.render('about');
+});
+
+app.get("/weather", (req,res) =>{
+    res.render('weather');
+});
+
+app.get("*", (req,res) =>{
+    res.render('404error',{
+        errorMsg:'OppS! page Not Found'
+    })
+});
+
+
+app.listen(port,()=>{
+    console.log(`listening to the port ${port}`);
+});
