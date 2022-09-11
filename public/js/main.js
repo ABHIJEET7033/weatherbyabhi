3bfacf881adfd0d15513d80a27407d6cd4b2789b
Abhijeet <2872000abhijeet@gmail.com>
2022-09-12 00:30:37 +0530

    final

--- public/js/main.js
+++ public/js/main.js
@@ -1,13 +1,21 @@
 const cityName = document.getElementById('cityName');
 const submitBtn = document.getElementById('submitBtn');
 const city_name = document.getElementById('city_name');
-const temp=document.getElementById('temp');
+const temp_real_val=document.getElementById('temp_real_val');
 const temp_status=document.getElementById('temp_status');
+
+
+ const datahide  = document.querySelector('.middle_layer');
+
 const getInfo = async(event)=>{
     event.preventDefault();
     let cityVal=cityName.value;
     if(cityVal===""){
+        alert('Empty Field');
       city_name.innerText='plz enter  name before search';
+      datahide.classList.add('data_hide');
+      
+     
     }
     else
     {
@@ -18,11 +26,17 @@ const getInfo = async(event)=>{
             console.log(data);
             const arrData=[data];
             city_name.innerText=`${arrData[0].name},${arrData[0].sys.country}`;
-            temp.innerText=arrData[0].main.temp;
+            temp_real_val.innerText=arrData[0].main.temp;
             const temp_status=arrData[0].weather[0].main;
+            temp_status.innerText=arrData[0].main.temp;
+
+            datahide.classList.remove('data_hide');
         }
         catch{
+        
             city_name.innerText='plz enter  correct name';
+            datahide.classList.add('data_hide');
+            
         }
     }
 }
